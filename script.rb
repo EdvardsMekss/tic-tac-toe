ac6183ef806127d89763af4b8f52a41a40ddb95b
EdvardsMekss <edvards.mekss@gmail.com>
2023-04-01 12:34:36 +0300

    Add make_move method

--- script.rb
+++ script.rb
@@ -1,17 +1,25 @@
 class Board
-    attr_accessor :fields
-  
-    def initialize
-      @fields = Array.new(3) { Array.new(3, '-') }
-    end
-  
-    def print_board
-      fields.each do |row|
-        puts row.join(' ')
-      end
+  attr_accessor :fields
+
+  def initialize
+    @fields = Array.new(3) { Array.new(3, '-') }
+  end
+
+  def print_board
+    fields.each do |row|
+      puts row.join(' ')
     end
   end
   
-  new_board = Board.new
-  new_board.print_board
+  def make_move(row, column, char)
+    fields[row][column] = char
+  end
   
+
+
+end
+  
+  new_board = Board.new
+  new_board.make_move(1, 2, 'X')
+  new_board.make_move(1, 1, 'X')
+  new_board.print_board
